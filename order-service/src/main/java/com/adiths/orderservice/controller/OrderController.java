f2b4cd3cbc89755502f094375a1266ab06a81ceb
Akash Pambhar <akash.pambhar@yahoo.com>
2023-08-05 21:02:09 +0530

    implement circuit-breaker with Resilience4j

--- order-service/src/main/java/com/adiths/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/adiths/orderservice/controller/OrderController.java
@@ -3,11 +3,16 @@ package com.adiths.orderservice.controller;
 import com.adiths.orderservice.dto.OrderRequest;
 import com.adiths.orderservice.dto.OrderResponse;
 import com.adiths.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -22,7 +27,15 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest) {
-        return orderService.placeOrder(orderRequest);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<ResponseEntity<String>> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<ResponseEntity<String>> placeOrderFallback(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() ->
+                new ResponseEntity<>("Something went wrong. Order is not placed. Please try again in some time", HttpStatus.SERVICE_UNAVAILABLE));
     }
 }
